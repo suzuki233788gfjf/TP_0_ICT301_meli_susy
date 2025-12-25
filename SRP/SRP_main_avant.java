f36c610679f1d5a0d0e8e7cacf9a399b3f37a327
suzuki <susymeli25@gmail.com>
2025-12-26 00:25:50 +0100

    Add main class to demonstrate Book usage

---
+++ SRP/SRP_main_avant.java
@@ -0,0 +1,10 @@
+// Utilisation de la classe Book
+public class Main {
+public static void main(String[] args) {
+ Book book = new Book("Les principes SOLID", "Etudiants de M1-GL", "Révision des
+principes SOLID");
+ book.printToScreen();
+ book.saveToDatabase();
+ book.emprunter("Marcial");
+ }
+}

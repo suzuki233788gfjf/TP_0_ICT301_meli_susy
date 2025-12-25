b13736868669dec49507c759863ff835ac07e479
suzuki <susymeli25@gmail.com>
2025-12-26 00:34:46 +0100

    Add BookSRP class to manage book data

---
+++ SRP/refractoring_classBookSRP.java
@@ -0,0 +1,16 @@
+ // Gère les données du livre
+ public class BookSRP {
+ private String title;
+ private String author;
+ private String content;
+public BookSRP(String title, String author, String content) {
+ this.title = title;
+ this.author = author;
+ this.content = content;
+ }
+
+// Méthodes pour accéder aux données (getters)
+ public String getTitle() { return title; }
+ public String getAuthor() { return author; }
+ public String getContent() { return content; }
+ }

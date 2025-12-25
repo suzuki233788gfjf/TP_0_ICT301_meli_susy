250e10e53eeed529ede429dd4d75f177799eee54
suzuki <susymeli25@gmail.com>
2025-12-25 23:32:29 +0100

    Add Employe class with salary calculation and save methods

---
+++ SRP/SRP_avant.java
@@ -0,0 +1,8 @@
+public class Employe {
+  public void calculerSalaire() {
+    system.out.println("calcul du salaire");
+  }
+  public void  sauvegarderEmployer() {
+    system.out.println("sauvegarde de l'employé dans la base de données");
+  }
+}  

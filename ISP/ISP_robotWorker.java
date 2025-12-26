4a7f4ffa418e5bd8f68c8e5db877b436d943874c
suzuki <susymeli25@gmail.com>
2025-12-26 01:16:21 +0100

    Implement RobotWorker class with work and eat methods

---
+++ ISP/ISP_robotWorker.java
@@ -0,0 +1,11 @@
+class RobotWorker implements Worker {
+  @Override
+ public void work() {
+   System.out.println("Les Robots travaillent sans fatigue");
+ }
+@Override
+  public void eat() {
+    System.out.println("l'on ne doit pas faire manger un robot");
+    throw new UnsupportedOperationException("Les Robots ne mangent pas");
+  }
+ }

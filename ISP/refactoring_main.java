d24599b31e86b35f469e8b0a66255ceecf596dd8
suzuki <susymeli25@gmail.com>
2025-12-26 01:20:56 +0100

    Add MainISP class with main method

---
+++ ISP/refactoring_main.java
@@ -0,0 +1,10 @@
+public class MainISP {
+  public static void main(String[] args) {
+    HumanWorker human = new HumanWorker();
+    human.work();
+    human.eat();
+    
+RobotWorker robot = new RobotWorker();
+    robot.work();
+  }
+}

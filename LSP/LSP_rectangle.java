272b92ab7aefbd1443f3282a21abd01b6ea4efdf
suzuki <susymeli25@gmail.com>
2025-12-26 01:01:07 +0100

    Add Rectangle class with area calculation

---
+++ LSP/LSP_rectangle.java
@@ -0,0 +1,11 @@
+class Rectangle {
+ protected int width;
+ protected int height;
+ public void setWidth(int width) {
+   this.width = width;
+ }
+ public void setHeight(int height) {
+   this.height = height; }
+ public int getArea() {
+   return width * height; }
+ }

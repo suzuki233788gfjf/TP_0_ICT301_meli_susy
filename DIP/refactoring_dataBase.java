6b963ec837359643a4cf3ca100af8bdd66b2089d
suzuki <susymeli25@gmail.com>
2025-12-26 01:33:04 +0100

    Refactor Database interface and MySQL implementation

---
+++ DIP/refactoring_dataBase.java
@@ -0,0 +1,9 @@
+ interface Database {
+   public void save(String data);
+ }
+class MySQLDatabase implements Database {
+  @Override
+  public void save(String data) {
+    System.out.println("Saving to MySQL: " + data);
+  }
+}

7f1e5c934b8f3f76d7271861c736e5d2a43752d8
suzuki <susymeli25@gmail.com>
2025-12-26 01:06:45 +0100

    Implement Square class for area calculation

---
+++ LSP/refactoring_square.java
@@ -0,0 +1,10 @@
+ class Square implements Shape {
+   private int side;
+   public Square(int side) {
+     this.side = side;
+   }
+@Override
+   public int getArea() {
+    return side * side;
+  }
+}

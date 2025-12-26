5390ca2d4beda65fbccba3c028552bbea5722ce8
suzuki <susymeli25@gmail.com>
2025-12-26 01:36:40 +0100

    Refactor main method for clarity

---
+++ DIP/reafactoring:main.java
@@ -0,0 +1,11 @@
+public class Main {
+  public static void main(String[] args) {
+    Database database;
+    database = new MySQLDatabase();
+    OrderProcessor order = new OrderProcessor(database);
+    order.processOrder("'Données à sauvegarder'");
+    database = new MongoDBDatabase();
+    OrderProcessor order1 = new OrderProcessor(database);
+    order1.processOrder("'Données à sauvegarder'");
+  }
+}
